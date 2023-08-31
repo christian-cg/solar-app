94824ab21b04c92c2cce56ff8372539244e7b255
christian castillo <christiancastillog@gmail.com>
2023-08-31 12:01:02 +0200

    Initial files and directories with previous progress

---
+++ app/app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import requests
+
+#url = st.secrets.url
+url = 'http://localhost:8501/predict'
+
+st.header("Solar panel condition classifier")
+st.text("""Upload an image to evaluate the condition of solar panels.""")
+
+# Allow users to upload images
+uploaded_file = st.file_uploader("Upload an image file:", type=["jpeg", "jpg", "png"])
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.read()
+    st.image(bytes_data)
+    st.write(bytes_data)
+
+    files = {"file": (bytes_data)}
+    response = requests.post(url, files=files)
+
+    if response.status_code == 200:
+        result = response.json()
+        st.success("Images uploeaded succesfully!")
+        st.write("API response: ", result)
+    else:
+        st.error("Images upload failed.")
+
+# Evaluate images and retrieve the model's classification results
+# if st.button("Evaluate"):
+#     pass
