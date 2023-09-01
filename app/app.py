ebbdb4ccb6707704cf987e5ad0ec5d6a7a323218
christian castillo <christiancastillog@gmail.com>
2023-09-01 09:20:17 +0200

    Updated the app to better connect with the API and accept multiple files

--- app/app.py
+++ app/app.py
@@ -1,30 +1,42 @@
 import streamlit as st
 import requests
+import pdb
 
-#url = st.secrets.url
+#url = st.secrets['url']
 url = 'http://localhost:8501/predict'
 
 st.header("Solar panel condition classifier")
 st.text("""Upload an image to evaluate the condition of solar panels.""")
 
 # Allow users to upload images
-uploaded_file = st.file_uploader("Upload an image file:", type=["jpeg", "jpg", "png"])
+uploaded_files = st.file_uploader("Upload an image file:",
+                                 type=['jpeg', 'jpg', 'png'],
+                                 accept_multiple_files=True)
 
-if uploaded_file is not None:
+for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.getvalue()
+
+if uploaded_files is not None:
+    files = {uploaded_file.name: (bytes_data)}
+
     st.image(bytes_data)
-    st.write(bytes_data)
+    st.write(uploaded_file.name)
+    #st.write(bytes_data)
 
-    files = {"file": (bytes_data)}
-    response = requests.post(url, files=files)
-
-    if response.status_code == 200:
-        result = response.json()
-        st.success("Images uploeaded succesfully!")
-        st.write("API response: ", result)
-    # else:
-    #     st.error("Images upload failed.")
+@breakpoint
 
 # Evaluate images and retrieve the model's classification results
-# if st.button("Evaluate"):
-#     pass
+if st.button("Evaluate"):
+    response = requests.post(url, files=files)
+
+    result = response.json()
+
+    st.write("Response status code: ", response.status_code)
+    st.write("API response: ", result)
+
+    # if response.status_code == 200:
+    #     result = response.json()
+    #     st.success("Images uploeaded succesfully!")
+    #     st.write("API response: ", result)
+    # else:
+    #     st.error("Images upload failed.")
