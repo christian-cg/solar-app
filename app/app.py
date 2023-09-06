9c1ed74445c5205bb4a1522e2dc1b04207e6e93c
christian castillo <christiancastillog@gmail.com>
2023-09-06 11:50:30 +0200

    Added a page header and 3 columns layout for displaying results.

--- app/app.py
+++ app/app.py
@@ -3,24 +3,45 @@ import pandas as pd
 import requests
 import ipdb
 
+# Page config
 st.set_page_config(
     page_title='Solar project',
     layout='wide'
 )
 
-# header_html = """
-# <div style="padding:0px; border:0px; border-radius:0px; background:url(https://images.unsplash.com/photo-1625301840055-7c1b7198cfc0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2942&q=80); height:200px;">
-#     <h1 style="color:#333;text-align:center;"></h1>
-# </div>
-# """
+# Custom CSS
+css = """
+<style>
+    .page-header {
+        height:128px;
+        margin: 0 0 0 0;
+        padding: 0 0 0 0;
+        border: 0;
+        border-radius: 16px 16px 0 0;
+        background-image: url(https://images.unsplash.com/photo-1625301840055-7c1b7198cfc0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2942&q=80);
+        background-position: center;
+    }
+    .css-z5fcl4 {
+        padding-top: 48px;
+    }
+</style>
+"""
 
-# st.markdown(header_html, unsafe_allow_html=True)
+st.markdown(css, unsafe_allow_html=True)
 
+# Custom HTML elements
+header_html = """
+<div class="page-header"></div>
+"""
+
+st.markdown(header_html, unsafe_allow_html=True)
+
+# Streamlit app
 #url = st.secrets['url']
-#url = 'http://localhost:8000/predict'
 url = 'https://solarpanelstatus-z7imutgpsq-ew.a.run.app/predict'
 
 st.header("Solar panel condition classifier")
+
 st.markdown("""
             Evaluate if a solar panel is **clean**, **damaged** or **dirty**
             """)
@@ -34,11 +55,12 @@ uploaded_files = st.file_uploader("Upload images:",
 if st.button("Evaluate images", type='primary'):
     # Check if any files where uploaded
     if uploaded_files:
-        col_header_1, col_header_2 = st.columns(2)
+        col_header_1, col_header_2, col_header_3 = st.columns(3)
         with col_header_1:
             st.markdown('**Image**')
         with col_header_2:
             st.markdown('**Class**')
+
         # Fetch the model's classification results
         for uploaded_file in uploaded_files:
             bytes_data = uploaded_file.getvalue()
@@ -46,23 +68,12 @@ if st.button("Evaluate images", type='primary'):
             response = requests.post(url, files=files)
             if response.status_code == 200:
                 result = response.json()['predition'] # Warning: this typo is in the API deployed to the cloud. But fixed and merged on GH repo.
-                col1, col2 = st.columns(2)
+                col1, col2, col3 = st.columns(3)
                 with col1:
-                    st.image(bytes_data, caption=uploaded_file.name, width=200)
+                    st.image(bytes_data, caption=uploaded_file.name, width=256)
                 with col2:
                     st.markdown(f"The image was classified as **{result}**.")
             else:
                 st.error(f"Error uploading the image: {uploaded_file.name}")
     else:
         st.warning("You need to upload an image to be evaluated.")
-
-
-
-### Single image upload ###
-
-# response = requests.post(url, files={"image": ("image.jpg", bytes_data, "image/jpeg")})
-
-# if uploaded_files is not None:
-#     files = {uploaded_file.name: (bytes_data)}
-#     st.image(bytes_data)
-#     st.write(bytes_data)
